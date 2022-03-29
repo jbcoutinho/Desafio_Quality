fa71ead6af200ffb0a0870d74711dda23cbcc925
itIsV <soicinivt@gmail.com>
2022-03-29 18:41:15 -0300

    feat: start api for tests.

---
+++ src/main/java/com/mercadolivre/desafio_quality/controller/GroundController.java
@@ -0,0 +1,34 @@
+package com.mercadolivre.desafio_quality.controller;
+
+import com.mercadolivre.desafio_quality.model.Ground;
+import com.mercadolivre.desafio_quality.model.Room;
+import com.mercadolivre.desafio_quality.model.dto.RoomDTO;
+import com.mercadolivre.desafio_quality.service.GroundService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+
+@RestController
+public class GroundController {
+    @Autowired
+    GroundService service;
+
+    @PostMapping("/ground")
+    public ResponseEntity<BigDecimal> groundValue(@RequestBody Ground ground) {
+
+        return ResponseEntity.ok(service.groundValue(ground)) ;
+
+    }
+
+    @PostMapping("/ground/biggest")
+    public ResponseEntity<RoomDTO> biggestArea(@RequestBody Ground ground) {
+
+        return ResponseEntity.ok(service.biggestArea(service.roomsArea(ground))) ;
+
+    }
+}
--- src/main/java/com/mercadolivre/desafio_quality/service/GroundService.java
+++ src/main/java/com/mercadolivre/desafio_quality/service/GroundService.java
@@ -2,12 +2,15 @@ package com.mercadolivre.desafio_quality.service;
 
 import com.mercadolivre.desafio_quality.model.Ground;
 import com.mercadolivre.desafio_quality.model.Room;
+import com.mercadolivre.desafio_quality.model.dto.RoomDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class GroundService {
@@ -15,11 +18,37 @@ public class GroundService {
     @Autowired
     RoomService roomService;
 
-    public HashMap roomsArea(Ground ground) {
+    public HashMap<String, Double> roomsArea(Ground ground) {
         HashMap<String, Double> objectObjectHashMap = new HashMap<>();
         for (Room room : ground.getRooms()) {
                 objectObjectHashMap.put(room.getRoomName(), roomService.calculaArea(room));
             }
         return objectObjectHashMap;
     }
-}
+
+    private Double calculateArea(HashMap<String, Double> hashMap) {
+        return hashMap.values().stream().reduce(0.0, (accumulator, element) -> accumulator + element);
+    }
+
+    public BigDecimal groundValue(Ground ground) {
+
+        HashMap<String, Double> hashMap = this.roomsArea(ground);
+        Double value = calculateArea(hashMap);
+
+        return ground.getDistrict().getValueDistrictM2().multiply(BigDecimal.valueOf(value));
+
+    }
+
+    public RoomDTO biggestArea(HashMap<String, Double> rooms) {
+
+        List<Double> areas = rooms.values().stream().sorted().collect(Collectors.toList());
+        Double issoAaqui = areas.get(areas.size() -1);
+            for (String o : rooms.keySet()) {
+                if (rooms.get(o).equals(issoAaqui)) {
+                    return new RoomDTO(o, issoAaqui);
+                }
+            }
+            return null;
+        }
+    }
+
