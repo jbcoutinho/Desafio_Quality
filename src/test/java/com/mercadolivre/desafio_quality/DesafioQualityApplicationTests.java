2f96449d7bf8756ac71286cf97ecf64fab63983f
jbarcelos <joao.barcelos@mercadolivre.com>
2022-03-30 17:22:33 -0300

    test 3

--- src/test/java/com/mercadolivre/desafio_quality/DesafioQualityApplicationTests.java
+++ src/test/java/com/mercadolivre/desafio_quality/DesafioQualityApplicationTests.java
@@ -1,13 +1,19 @@
 package com.mercadolivre.desafio_quality;
 
+import com.mercadolivre.desafio_quality.service.GroundService;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DesafioQualityApplicationTests {
 
+    @InjectMocks
+    private GroundService service;
+
     @Test
-    void contextLoads() {
+    void groundTest() {
+        service.groundArea(10L);
     }
 
 }
--- src/test/java/com/mercadolivre/desafio_quality/GroundServiceTest.java
+++ src/test/java/com/mercadolivre/desafio_quality/GroundServiceTest.java
@@ -9,50 +9,55 @@ import com.mercadolivre.desafio_quality.service.GroundService;
 import com.mercadolivre.desafio_quality.service.RoomService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 
 @SpringBootTest
-@AutoConfigureMockMvc
-@ExtendWith(MockitoExtension.class)
 public class GroundServiceTest {
 
     @InjectMocks
     GroundService groundService;
 
-//    @Mock
-//    RoomService roomService;
+    @Mock
+    RoomService roomService;
 
     @Mock
     private GroundRepository repository;
 
-    private List<RoomDTO> mockGround(){
-        RoomDTO room1 = new RoomDTO("quarto", 5.0);
-        RoomDTO room2 = new RoomDTO("sala", 5.0);
-        RoomDTO room3 = new RoomDTO("cozinha", 5.0);
-        List<RoomDTO> rooms = new ArrayList<>();
+    private Ground mockGround(){
+
+
+        Room room1 = new Room(1L,"quarto", 5.0, 1.0);
+        Room room2 = new Room(1L,"sala", 5.0, 1.0);
+        Room room3 = new Room(1L,"cozinha", 5.0, 1.0);
+        List<Room> rooms = new ArrayList<>();
         rooms.add(room1);
         rooms.add(room2);
         rooms.add(room3);
-        return rooms;
+        return new Ground(1L,"name", new District(1L,"teste", BigDecimal.valueOf(15)), rooms);
     }
 
     @Test
     public void deveDevolverAAreaDeUmaPropriedade() {
-        Mockito.when(groundService.getListRoomWithCalculatedArea(Mockito.anyLong())).thenReturn(mockGround());
-        GroundService groundService = new GroundService(mockGround());
-        assertEquals(25.0, groundService.groundArea(10L));
+        Mockito.when(repository.findById(anyLong())).thenReturn(Optional.of(mockGround()));
+        Mockito.when(roomService.calculaArea(any(Room.class))).thenReturn(10.0);
+        Mockito.when(roomService.sumRoomsArea(any())).thenReturn(10.0);
+        assertEquals(10.0, groundService.groundArea(10L));
     }
 
 
