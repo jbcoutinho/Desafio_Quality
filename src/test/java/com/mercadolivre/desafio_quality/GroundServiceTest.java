99ff7843e36da9066d5f9bf6430943070c8ea14d
Juliana27 <juliana.brito@mercadolivre.com>
2022-03-30 16:20:11 -0300

    teste 3

--- src/test/java/com/mercadolivre/desafio_quality/GroundServiceTest.java
+++ src/test/java/com/mercadolivre/desafio_quality/GroundServiceTest.java
@@ -1,37 +1,59 @@
 package com.mercadolivre.desafio_quality;
 
+import com.mercadolivre.desafio_quality.model.District;
 import com.mercadolivre.desafio_quality.model.Ground;
 import com.mercadolivre.desafio_quality.model.Room;
+import com.mercadolivre.desafio_quality.model.dto.RoomDTO;
+import com.mercadolivre.desafio_quality.repository.GroundRepository;
 import com.mercadolivre.desafio_quality.service.GroundService;
 import com.mercadolivre.desafio_quality.service.RoomService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@SpringBootTest
+@AutoConfigureMockMvc
+@ExtendWith(MockitoExtension.class)
 public class GroundServiceTest {
 
-    GroundService groundService = new GroundService();
+    @InjectMocks
+    GroundService groundService;
+
+//    @Mock
+//    RoomService roomService;
 
     @Mock
-    RoomService roomService;
+    private GroundRepository repository;
 
-//    @Test
-//    public void deveRetornarUmaListaDeRooms() {
-//        Mockito.when(roomService.calculaArea(Mockito.any(Room.class))).thenReturn(12.0);
-//        Room room1 = new Room("cozinha", 15.0, 20.0);
-//        Room room2 = new Room("escritorio", 40.0, 20.0);
-//        Room room3 = new Room("sala", 25.0, 20.0);
-//        Ground ground = new Ground();
-//        List<Room> lista = new ArrayList<>();
-//        lista.add(room1);
-//        lista.add(room2);
-//        lista.add(room3);
-//        ground.setRooms(lista);
-//        groundService.roomsArea(ground);
-//
-//    }
+    private List<RoomDTO> mockGround(){
+        RoomDTO room1 = new RoomDTO("quarto", 5.0);
+        RoomDTO room2 = new RoomDTO("sala", 5.0);
+        RoomDTO room3 = new RoomDTO("cozinha", 5.0);
+        List<RoomDTO> rooms = new ArrayList<>();
+        rooms.add(room1);
+        rooms.add(room2);
+        rooms.add(room3);
+        return rooms;
+    }
+
+    @Test
+    public void deveDevolverAAreaDeUmaPropriedade() {
+        Mockito.when(groundService.getListRoomWithCalculatedArea(Mockito.anyLong())).thenReturn(mockGround());
+        GroundService groundService = new GroundService(mockGround());
+        assertEquals(25.0, groundService.groundArea(10L));
+    }
+
+
 }
