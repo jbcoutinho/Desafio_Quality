88d009cec41b25f49fedf13eba339a153d99a63f
jbarcelos <joao.barcelos@mercadolivre.com>
2022-03-31 13:34:04 -0300

    feat: adding rest controller advice

---
+++ src/main/java/com/mercadolivre/desafio_quality/controller/advice/ControllerAdvice.java
@@ -0,0 +1,26 @@
+package com.mercadolivre.desafio_quality.controller.advice;
+
+import com.mercadolivre.desafio_quality.model.dto.ErrorDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ControllerAdvice {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<ErrorDTO> handleValidationExceptions(MethodArgumentNotValidException e) {
+        ErrorDTO error = new ErrorDTO("MethodArgumentNotValidException", e.getBindingResult().getFieldError().getDefaultMessage());
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    protected ResponseEntity<ErrorDTO> handleValidationExceptions(HttpMessageNotReadableException e) {
+        ErrorDTO error = new ErrorDTO("HttpMessageNotReadableException", e.getMessage());
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+}
---
+++ src/main/java/com/mercadolivre/desafio_quality/model/dto/ErrorDTO.java
@@ -0,0 +1,15 @@
+package com.mercadolivre.desafio_quality.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorDTO {
+
+    private String name;
+    private String description;
+
+}
