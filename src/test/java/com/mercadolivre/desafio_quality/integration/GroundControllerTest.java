037504f33e0cbed0443ef34f3cceb4e00afa86c1
Andris Buscariolli <andris.buscariolli@mercadolivre.com>
2022-03-31 18:46:28 -0300

    add integration test to shouldGetRoomDTOList method

--- src/test/java/com/mercadolivre/desafio_quality/integration/GroundControllerTest.java
+++ src/test/java/com/mercadolivre/desafio_quality/integration/GroundControllerTest.java
@@ -2,8 +2,10 @@ package com.mercadolivre.desafio_quality;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mercadolivre.desafio_quality.model.Ground;
+import com.mercadolivre.desafio_quality.model.dto.RoomDTO;
 import com.mercadolivre.desafio_quality.repository.GroundRepository;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +13,9 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -24,6 +29,16 @@ public class GroundControllerTest {
 
     @Autowired
     private GroundRepository repository;
+
+    private void creatGround() throws Exception {
+        MvcResult mvcResult = mockMvc
+                .perform(MockMvcRequestBuilders.post("/ground")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(getObject()))
+                .andExpect(status().isCreated())
+                .andExpect(content().contentType("application/json"))
+                .andReturn();
+    }
 
     @Test
     public void shouldCreateAGroundAndReturn201() throws Exception {
@@ -42,10 +57,19 @@ public class GroundControllerTest {
         Ground ground = objectMapper.readValue(content, Ground.class);
 
         repository.deleteById(ground.getId());
-
     }
 
-    public String getObject(){
+    @Test
+    public void shouldGetRoomDTOList() throws Exception {
+        creatGround();
+        mockMvc
+            .perform(MockMvcRequestBuilders.get("/ground/{groundID}/room/areas", 1))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType("application/json"))
+            .andReturn();
+    }
+
+    private String getObject(){
         return "{\"propName\":\"Myname\",\"district\":{\"propDistrict\":9,\"valueDistrictM2\":10},\"rooms\":[{\"roomName\":\"Qualquercoisa\",\"roomLength\":10,\"roomWidth\":2},{\"roomName\":\"Qualquer\",\"roomLength\":10,\"roomWidth\":1},{\"roomName\":\"Outracoisa\",\"roomLength\":10,\"roomWidth\":1}]}";
     }
 
