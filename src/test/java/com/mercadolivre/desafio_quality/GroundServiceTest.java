25c15f2c40e9919c50e417d41ad2f3fa5265c6a1
renatograca <renatomacedo1995@gmail.com>
2022-03-31 11:31:34 -0300

    feat: teste districtService

--- src/test/java/com/mercadolivre/desafio_quality/GroundServiceTest.java
+++ src/test/java/com/mercadolivre/desafio_quality/GroundServiceTest.java
@@ -13,6 +13,7 @@ import com.mercadolivre.desafio_quality.model.dto.RoomInputDTO;
 import com.mercadolivre.desafio_quality.repository.GroundRepository;
 import com.mercadolivre.desafio_quality.repository.RoomRepository;
 import com.mercadolivre.desafio_quality.service.DistrictService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -30,7 +31,16 @@ import com.mercadolivre.desafio_quality.service.GroundService;
 @ExtendWith(MockitoExtension.class)
 class GroundServiceTest {
 
-    private GroundService groundService;
+    private GroundService service;
+    private final GroundRepository groundRepositoryMock = Mockito.mock(GroundRepository.class);
+    private final RoomRepository roomRepositoryMock = Mockito.mock(RoomRepository.class);
+    private final DistrictService districtServiceMock = Mockito.mock(DistrictService.class);
+
+
+    @BeforeEach
+    public void init() {
+        service = new GroundService(groundRepositoryMock, roomRepositoryMock,  districtServiceMock);
+    }
 
     @Test
     void shouldReturnRoomArea() {
@@ -62,7 +72,7 @@ class GroundServiceTest {
 //	}
     public Ground createGround() {
         Room roomQuarto = Room.builder().roomName("Quarto").roomLength(Double.valueOf(10)).roomWidth(Double.valueOf(5)).build();
-        Room roomSala = Room.builder().roomName("Sala").roomLength(Double.valueOf(10)).roomWidth(Double.valueOf(5)).build();
+        Room roomSala = Room.builder().roomName("Sala").roomLength(Double.valueOf(5)).roomWidth(Double.valueOf(5)).build();
 
         List<Room> rooms = new ArrayList();
         rooms.add(roomQuarto);
@@ -75,40 +85,38 @@ class GroundServiceTest {
 
     @Test
     public void verificaSeRetornaValorDoImovel() {
-        GroundRepository groundRepositoryMock = Mockito.mock(GroundRepository.class);
-        RoomRepository roomRepositoryMock = Mockito.mock(RoomRepository.class);
-
         Ground casaChique = createGround();
-        GroundService service = new GroundService(groundRepositoryMock, roomRepositoryMock, new DistrictService());
         Mockito.when(groundRepositoryMock.findById(Long.valueOf("1234"))).thenReturn(Optional.of(casaChique));
 
         BigDecimal result = service.groundValue(Long.valueOf("1234"));
 
-
-        assertEquals(BigDecimal.valueOf(120000.0), result);
+        assertEquals(BigDecimal.valueOf(90000.0), result);
     }
 
     @Test
     public void verificaSeRetornaErrorAoConsultarValorDeImovelInexistente() {
-        GroundRepository groundRepositoryMock = Mockito.mock(GroundRepository.class);
-        RoomRepository roomRepositoryMock = Mockito.mock(RoomRepository.class);
 
-        GroundService service = new GroundService(groundRepositoryMock, roomRepositoryMock, new DistrictService());
         Mockito.when(groundRepositoryMock.findById(Long.valueOf("1234"))).thenReturn(Optional.empty());
 
         Throwable exception = assertThrows(RuntimeException.class,()-> service.groundValue(Long.valueOf("1234")));
         assertEquals("imovel nao encontrado na nossa base de dados!", exception.getMessage() );
     }
 
+    @Test
+    public void verificaSeRetornaMaiorArea() {
+
+        Ground ground = createGround();
+        List<RoomDTO> roomDTOS= new ArrayList<>();
+        for (Room room: ground.getRooms()) {
+            roomDTOS.add(new RoomDTO(room.getRoomName(), Double.valueOf(room.getRoomLength() * room.getRoomWidth())));
+        }
+        RoomDTO roomDTO = service.biggestRoom(roomDTOS);
+        assertEquals(roomDTOS.get(0), roomDTO);
+    }
+
 
     @Test
 	public void verificaSeSalvaUmRoom() {
-        RoomRepository roomRepositoryMock = Mockito.mock(RoomRepository.class);
-        GroundRepository groundRepositoryMock = Mockito.mock(GroundRepository.class);
-        DistrictService districtServiceMock = Mockito.mock(DistrictService.class);
-
-        GroundService service = new GroundService(groundRepositoryMock, roomRepositoryMock, districtServiceMock);
-
         Ground ground = createGround();
         DistrictDTO districtDTO = new DistrictDTO(ground.getDistrict().getPropDistrict(), ground.getDistrict().getValueDistrictM2());
         List<RoomInputDTO> roomInputDTO = new ArrayList<>();
